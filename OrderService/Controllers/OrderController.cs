a36487d03da5441e9b09ab248ae7c3be5f51d902
Arafat Zaman Anik <arafat.zaman.anik@gmail.com>
2024-08-21 18:03:39 +0600

    Add project files.

---
+++ OrderService/Controllers/OrderController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OrderService.Models;
+using System.Collections.Generic;
+
+namespace OrderService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class OrderController : ControllerBase
+    {
+        private static List<Order> _orders = new List<Order>();
+
+        [HttpGet]
+        public IEnumerable<Order> Get() => _orders;
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Order order)
+        {
+            _orders.Add(order);
+            return Ok();
+        }
+    }
+}
---
+++ ProductService/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductService.Models;
+using System.Collections.Generic;
+
+namespace ProductService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private static List<Product> _products = new List<Product>
+        {
+            new Product { Id = 1, Name = "Laptop", Price = 1200 },
+            new Product { Id = 2, Name = "Smartphone", Price = 800 }
+        };
+
+        [HttpGet]
+        public IEnumerable<Product> Get() => _products;
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Product product)
+        {
+            _products.Add(product);
+            return Ok();
+        }
+    }
+}
